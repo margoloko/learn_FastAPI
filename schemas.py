cbf9ea4def83a3f4887df4d16c03aac106ac7241
1 <margoloko@mail.ru>
2023-03-31 18:27:35 +0700

    add validators

--- schemas.py
+++ schemas.py
@@ -1,6 +1,8 @@
+import re
+
 from enum import Enum
 from typing import Optional, Dict, List, Union
-from pydantic import BaseModel
+from pydantic import BaseModel, Field, root_validator, validator
 
 
 class EducationLevel(str, Enum):
@@ -10,8 +12,32 @@ class EducationLevel(str, Enum):
 
 
 class Person(BaseModel):
-    name: str
-    surname: Union[str, List[str]]
-    age: Optional[int]
-    is_staff: bool = False
+    name: str = Field(...,
+                      max_lenght=50,
+                      title='Полное имя',
+                      description='Можно вводить в любом регистре')
+    surname: Union[str, List[str]] = Field(...,                                           
+                                           max_lenght=50)
+    age: Optional[int] = Field(None, gt=4, le=99)
+    is_staff: bool = Field(False, alias='is-staff')
     education_level: Optional[EducationLevel]
+
+
+    class Config:
+        title = 'Класс для приветствия'
+        min_anystr_length = 2
+
+    @validator('name')
+    def name_cant_be_numeric(cls, value: str):
+        if value.isnumeric():
+            raise ValueError('not chislo')
+        return value
+    
+    @root_validator(skip_on_failure=True)
+    def using_different_languages(cls, values):
+        surname = ''.join(values['surname'])
+        checked_value = values['name'] + surname
+        if (re.search('[а-я]', checked_value, re.IGNORECASE)
+        and re.search('[a-z]', checked_value, re.IGNORECASE)):
+            raise ValueError('tolko latin or cirill')
+        return values
